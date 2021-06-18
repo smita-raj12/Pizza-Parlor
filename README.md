858e8d515e8c0a20ed2a2efdd2f26c92050f9103
smita Raj <smita.raj12@gmail.com>
2021-06-18 09:01:17 -0700

    created objects on pizza function

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ website for a pizza company where a user can choose one or more individual toppi
 Allow the user to choose toppings and size for the pizza they'd like to order.
 Create a pizza object constructor with properties for toppings and size.
 Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
+
+## Description: pizzaParlor()
+
--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,47 @@ pizzaParlor.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 };
+
+pizzaParlor.prototype.findPizza = function(id) {
+  if (this.pizzas[id] != undefined) {
+    return this.pizzas[id];
+  }
+  return false;
+};
+
+function Pizza(fullname,topings,size){
+this.fullname = fullname;
+this.topings = topings;
+this.sizes = sizes
+}
+
+function Sizes(){
+   this.sizes = {};
+   this.currentId = 0 
+}
+Sizes.prototype.addSize = function(size) {
+  size.id = this.assignId();
+  this.sizes[size.id] = size;
+};
+
+Sizes.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+Sizes.prototype.findSize = function(id) {
+  if (this.sizes[id] != undefined) {
+    return this.sizes[id];
+  }
+  return false;
+};
+
+function Size(sizeType,cost){
+  this.sizeType = sizeType;
+  this.cost = cost;
+}
+
+function Topings(){
+    this.topings = {};
+    this.currentId = 0;
+}
