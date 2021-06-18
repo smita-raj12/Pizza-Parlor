38b3bd1bc637518f10ad38c7ab95db337118552a
smita Raj <smita.raj12@gmail.com>
2021-06-18 10:17:23 -0700

    added size and topings functions to pizza parlor function

--- js/scripts.js
+++ js/scripts.js
@@ -27,10 +27,21 @@ this.topings = topings;
 this.sizes = sizes;
 }
 
+function displayPizzaDetails(pizzaToDisplay) {
+  let pizza = $("ul#Pizzas");
+  let htmlForPizza = "";
+  Object.keys(pizzaToDisplay.pizzas).forEach(function(key) {
+    const pizza = pizzaToDisplay.findToping(key);
+    htmlForPizza += "<li id=" + pizza.id + ">" + pizza.fullname +  "</li>";
+  });
+  pizza.html(htmlForPizza);
+}
+
 function Sizes(){
   this.sizes = {};
   this.currentId = 0 
 }
+
 Sizes.prototype.addSize = function(size) {
   size.id = this.assignId();
   this.sizes[size.id] = size;
@@ -51,6 +62,16 @@ Sizes.prototype.findSize = function(id) {
 function Size(sizeType,cost){
   this.sizeType = sizeType;
   this.cost = cost;
+}
+
+function displaySizeDetails(sizeToDisplay) {
+  let pizzaSize = $("ul#sizes");
+  let htmlForSize = "";
+  Object.keys(sizeToDisplay.sizes).forEach(function(key) {
+    const size = sizeToDisplay.findSize(key);
+    htmlForSize += "<li id=" + size.id + ">" + size.sizeType + " " + size.cost +  "</li>";
+  });
+  pizzaSize.html(htmlForSize);
 }
 
 function Topings(){
@@ -80,3 +101,28 @@ function Toping(TopingType,cost){
   this.cost = cost;
 }
 
+function displayTopingDetails(topingToDisplay) {
+  let pizzaToping = $("ul#Topings");
+  let htmlForToping = "";
+  Object.keys(topingToDisplay.topings).forEach(function(key) {
+    const toping = topingToDisplay.findToping(key);
+    htmlForToping += "<li id=" + toping.id + ">" + toping.topingType + " " + toping.cost +  "</li>";
+  });
+  pizzaToping.html(htmlForToping);
+}
+
+function showPizza(pizzaId) {
+  const pizza = pizzaParlor.findPizza(pizzaId);
+  $("#show-pizza").show();
+  $(".fname").html(pizza.fullName);
+  $(".phonenum").html(pizza.phoneno);
+  $(".sizes").html(displaySizeDetails(pizza.sizes));
+  $(".topings").html(displayTopingDetails(pizza.topings));
+}
+
+function attachPizzaListeners() {
+  $("ul#Pizzas").on("click", "li", function() {
+    showContact(this.id);
+  });
+  displayPizzaDetails(pizzaParlor)
+}
